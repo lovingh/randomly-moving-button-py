bd5f89e9b02e81fa6453906c90e2ee336111ab8f
lovingh <156435835+lovingh@users.noreply.github.com>
2024-01-16 12:32:23 +0700

    Update and rename main.py to Sasa.py

--- Sasa.py
+++ Sasa.py
@@ -7,7 +7,7 @@ class App(tk.Tk):
         super().__init__(*args, **kwargs)
         self.title("Do you love me?")
         self.geometry("400x200")
-        self.message_label = tk.Label(self, text="Do you love me?", font=("Helvetica", 16))
+        self.message_label = tk.Label(self, text="Do you love me jemario?", font=("Helvetica", 16))
         self.message_label.pack(pady=20)
         self.yes_button = tk.Button(self, text="Yes", command=self.show_love)
         self.no_button = tk.Button(self, text="No")
@@ -16,7 +16,7 @@ class App(tk.Tk):
         self.no_button.bind("<Enter>", self.move_no_button)
 
     def show_love(self):
-        self.message_label.config(text="I love you too! 😘")
+        self.message_label.config(text="i love your more! 😘")
 
     def move_no_button(self, event):
         x = random.randint(10, 350)
